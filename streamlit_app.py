e059aa79836f032f6241c3b88c5fdfff6e9981b7
padhyesd0609 <133854754+padhyesd0609@users.noreply.github.com>
2023-05-17 13:42:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Rava Dosa')
+streamlit.text('Tomato omlette')
+streamlit.text('Vada Pav')
+streamlit.text('Pav Bhaji')
+streamlit.text('Burger')
